b4a31198169d8c47cac1044636cd5899b76764ee
Diego Mariño <diegomarino@users.noreply.github.com>
2026-03-07 16:29:38 +0100

    chore: update takeout-photos to v1.0.3

--- Formula/takeout-photos.rb
+++ Formula/takeout-photos.rb
@@ -1,11 +1,11 @@
 class TakeoutPhotos < Formula
   desc "Process and organize Google Photos Takeout exports with metadata preservation"
   homepage "https://github.com/diegomarino/takeout-photos"
-  version "1.0.2"
+  version "1.0.3"
 
   on_arm do
     url "https://github.com/diegomarino/takeout-photos/releases/download/v#{version}/takeout-photos-#{version}-macos-arm64.tar.gz"
-    sha256 "1b93c4ce0b00d0753fd66d93e77eebd161bd12fa3e7f1d28a97a279f2020c8cd"
+    sha256 "29ea43b55ae5ec4cef8d62bd4a1b3629531e10658537ad6b2f79f7948e20c1ce"
   end
 
   def install
